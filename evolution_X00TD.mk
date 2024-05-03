ca4ef1b9d8209d8ff8d353cecd2f4a8397321e00
Tiktodz <ewprjkt@proton.me>
2024-05-03 12:06:05 +0000

    X00TD: Evolution-X bringup
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- evolution_X00TD.mk
+++ evolution_X00TD.mk
@@ -8,14 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution X.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# EvolutionX Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# EvolutionX GApps
+TARGET_USES_MINI_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := evolution_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -24,8 +32,8 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=ASUS_X00T \
-    PRODUCT_NAME=WW_X00T \
+    PRODUCT_DEVICE=ASUS_X00TD \
+    PRODUCT_NAME=WW_X00TD \
     PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
 
 # Build fingerprint
