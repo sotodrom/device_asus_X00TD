959f1575f66f4e5d58bb5b152509e6246786e31d
Meisie Widuri <ewprjkt@proton.me>
2024-05-10 04:01:35 +0700

    X00TD: Update vendorsetup.sh
    Change-Id: Id605f08aad2e0f88934251f8151960ed7ac28cff

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,9 @@
 rm -rf kernel/asus/sdm660
+rm -rf device/asus/sdm660-common
+
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 14-ksu kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU/userspace
+
+git clone --depth=1 https://github.com/sotodrom/device_asus_sdm660-common -b evo device/asus/sdm660-common
 
 export TZ=Asia/Jakarta
-
