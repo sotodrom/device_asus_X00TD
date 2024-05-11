1c9ad6fd8e43d198f1746ec44d76cec7855d859e
Meisie Widuri <ewprjkt@proton.me>
2024-05-12 03:01:36 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,4 +2,9 @@ rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 14-ksu kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU/userspace
 
+cd frameworks/base
+git reset --merge 2aecdd6
+git reset --merge HEAD~1
+cd ../../
+
 export TZ=Asia/Jakarta
