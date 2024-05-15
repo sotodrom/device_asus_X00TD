0babb03c861ef66a9e3b453ca351801b11a8f57f
Meisie Widuri <ewprjkt@proton.me>
2024-05-15 11:58:06 +0700

    X00TD: reset bionic

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,4 +6,7 @@ rm -rf device/asus
 git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b 14 device/asus/X00TD
 git clone --depth=1 https://github.com/sotodrom/device_asus_sdm660-common -b evo device/asus/sdm660-common
 
+rm -rf bionic
+git clone --depth=1 https://github.com/sotodrom/bionic -b 14 bionic
+
 export TZ=Asia/Jakarta
