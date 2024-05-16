42d30488040fd16b69bd11c6be0ad96446990d0d
Tiktodz <ewprjkt@proton.me>
2024-05-16 18:53:48 +0000

    X00TD: Update vendorsetup
    Change-Id: If664733232fbfc0561da68012912dc8e635e48b1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,4 +5,7 @@ rm -rf kernel/asus/sdm660/KernelSU/userspace
 rm -rf device/asus/X00TD
 git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b ursa device/asus/X00TD
 
+rm -rf frameworks/base
+git clone --depth=1 https://github.com/sotodrom/android_frameworks_base -b ursa frameworks/base
+
 export TZ=Asia/Jakarta
