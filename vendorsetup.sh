583cbe8b088ba3fb723b7ffa0430b1fe0238dd6d
Tiktodz <ewprjkt@proton.me>
2024-05-16 19:08:34 +0000

    X00TD: Redefine vt from manifests
    Change-Id: Ibc9e9881d18969a255c38977ad887918a91551f9

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,4 @@ rm -rf kernel/asus/sdm660/KernelSU/userspace
 rm -rf device/asus
 git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b u device/asus/X00TD
 
-rm -rf vendor/asus
-git clone --depth=1 https://github.com/sotodrom/vendor_asus -b u vendor/asus
-
 export TZ=Asia/Jakarta
