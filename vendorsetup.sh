b8c9b3a2d1f6c61fa4b0ffd6e35a2d0e55941e0a
Tiktodz <ewprjkt@proton.me>
2024-05-20 16:43:06 +0000

    X00TD: Regenerate
    Change-Id: I2afd75109d46a5df77337deefc09728d51f80312

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,19 @@ git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_s
 rm -rf kernel/asus/sdm660/KernelSU/userspace
 
 rm -rf device/asus
-git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b 14 device/asus/X00TD
+git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b los device/asus/X00TD
 git clone --depth=1 https://github.com/sotodrom/device_asus_sdm660-common -b evo device/asus/sdm660-common
+
+rm -rf build/soong
+git clone --depth=1 https://github.com/sotodrom/android_build_soong -b 14 build/soong
+
+rm -rf hardware/lineage/interfaces
+git clone --depth=1 https://github.com/sotodrom/android_hardware_lineage_interfaces -b 14 hardware/lineage/interfaces
+
+rm -rf device/lineage/sepolicy
+git clone --depth=1 https://github.com/sotodrom/android_device_lineage_sepolicy -b 14 device/lineage/sepolicy
+
+rm -rf device/qcom/sepolicy-legacy-um
+git clone --depth=1 https://github.com/sotodrom/android_device_qcom_sepolicy -b lineage-21.0-legacy-um device/qcom/sepolicy-legacy-um
 
 export TZ=Asia/Jakarta
