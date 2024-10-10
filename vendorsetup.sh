88428477073910a4e035148850dfca8a55a42837
Meisie Widuri <ewprjkt@proton.me>
2024-10-11 01:10:17 +0700

    X00TD: Drop common tree to avoid build conflicts

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ rm -rf kernel/asus/sdm660
 git clone --depth=1 https://github.com/texascake/android_kernel_asus_sdm660 -b 14 kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU/userspace
 
+rm -rf device/asus/sdm660-common
 rm -rf device/asus/X00TD
 git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b ursa device/asus/X00TD
 
