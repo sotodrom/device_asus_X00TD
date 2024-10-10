9d29512f0a1933d20f1226ce2a4b953a2fae9fc9
Tiktodz <dotkit@electrowizard.me>
2024-10-10 14:19:02 +0000

    X00TD: Regenerate
    Change-Id: I55edb708e6a50c25daa74c40b5b45821761d62d5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 14-ksu kernel/asus/sdm660
+git clone --depth=1 https://github.com/texascake/android_kernel_asus_sdm660 -b 14 kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU/userspace
 
 rm -rf device/asus/X00TD
 git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b ursa device/asus/X00TD
 
-rm -rf frameworks/base
-git clone --depth=1 https://github.com/sotodrom/android_frameworks_base -b ursa frameworks/base
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/sotodrom/vendor_asus -b ursa vendor/asus
 
 export TZ=Asia/Jakarta
