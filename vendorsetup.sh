44ed4a1f2fb352668b78db9b6033994e1accc030
Kneba <abenkenary3@gmail.com>
2025-08-28 14:37:09 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,8 +3,10 @@
 rm -rf kernel/asus/sdm660
 git clone --depth=1 https://github.com/Tiktodz/android_kernel_asus_sdm636 -b cip kernel/asus/sdm660
 
-#rm -rf device/asus
-git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b los device/asus/X00TD
+#rm -rf device/asus/X00TD
+#git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b los device/asus/X00TD
+
+rm -rf device/asus/sdm660-common
 git clone --depth=1 https://github.com/sotodrom/device_asus_sdm660-common -b evo device/asus/sdm660-common
 
 rm -rf hardware/qcom-caf/msm8998/audio
