6a2c70a266134449eb51ad5b81b3e6e41675cbda
ikwfahmi <mikhwanfahmi4@gmail.com>
2026-01-26 20:26:39 +0700

    X00TD: Decommonize some props
    Signed-off-by: ikwfahmi <mikhwanfahmi4@gmail.com>

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -9,25 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common crDroid stuff
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from X00TD device
+# Inherit from X00TD device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-TARGET_BUILD_DEVICE_AS_WEBCAM := true
-
-# Inherit some common device props
-WITH_GMS := false
-TARGET_HAS_UDFPS := false
-TARGET_ENABLE_BLUR := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
+# Inherit some flags.
+FULL_SYSTEM_OPTIMIZE_JAVA := true
 SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
 TARGET_EXCLUDES_AUDIOFX := true
-TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
 
-# Device identifier. This must come after all inclusions.
+# Device identifier
+# This must come after all inclusions.
 PRODUCT_NAME := lineage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
@@ -37,7 +33,4 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="sdm660_64-user 10 QKQ1 72 release-keys" \
-    DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
-    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
-    BuildFingerprint=asus/RU_X00TD/ASUS_X00T_6:10/QKQ1/17.2017.2012.438-20201203:user/release-keys
+    DeviceProduct=ASUS_X00TD
