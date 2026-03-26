2406e86a09d04a25bf4db2f281c642e5f0cdb242
Tiktodz <dotkit@electrowizard.me>
2026-03-26 13:53:53 +0000

    X00TD: Voltageify
    Change-Id: I8ac0605bf21b667755c889b280aa721403fe0c65
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- voltage_X00TD.mk
+++ voltage_X00TD.mk
@@ -9,21 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from X00TD device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some flags.
+# Inherit some VoltageOS flags.
 SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
-TARGET_EXCLUDES_AUDIOFX := true
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_INCLUDE_ACCORD := false
 
 # Device identifier
 # This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := voltage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
